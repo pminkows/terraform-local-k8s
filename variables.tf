47a8ec0c20591732a7299aca0e94c40603221537
Piotr Mińkowski <pminkows@redhat.com>
2022-11-22 11:27:47 +0100

    adding configuration

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "cluster-context" {
+  type    = string
+  default = "default/api-cluster-rh1bc0-gcp-redhatworkshops-io:6443/admin"
+}
---
+++ vault-ocp/variables.tf
@@ -0,0 +1,4 @@
+variable "cluster-context" {
+  type    = string
+  default = "default/api-cluster-7h9vm-7h9vm-sandbox240-opentlc-com:6443/opentlc-mgr"
+}
