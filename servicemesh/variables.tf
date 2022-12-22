aac93bae033a80876afb70b611e56a99993fefe1
pminkows <pminkows@redhat.com>
2022-12-22 13:19:27 +0100

    autorskie-12.2022

---
+++ servicemesh/variables.tf
@@ -0,0 +1,9 @@
+variable "cluster-context" {
+  type    = string
+  default = "default/api-cluster-n2276-n2276-sandbox1314-opentlc-com:6443/opentlc-mgr"
+}
+
+variable "domain" {
+  type    = string
+  default = "cluster-n2276.n2276.sandbox1314.opentlc.com"
+}
